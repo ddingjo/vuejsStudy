cac04dacd25cca31ad70474f58d2958e4182d0d7
ddingjo <ddingjo@gmail.com>
2023-07-24 00:20:35 +0900

    Navbar 컴포넌트로 이동

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light container">
-      <router-link class="navbar-brand" to="/">Navbar</router-link>
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link class="nav-link" :to="{name : 'Home'}">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/todos">Todos</router-link>
-        </li>
-      </ul>
-    </nav>
+  <Navbar />
   <div class="container">
     <router-view />
   </div>
@@ -18,10 +8,12 @@
 
 <script>
 import Toast from '@/components/Toast.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   components: {
-    Toast
+    Toast,
+    Navbar
   },
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container">
+      <router-link class="navbar-brand" to="/">Navbar</router-link>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" :to="{name : 'Home'}">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/todos">Todos</router-link>
+        </li>
+      </ul>
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
